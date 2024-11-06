82885fce503159f834ef641c9858c46714b59c7e
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2024-11-06 09:01:22 +0000

    lambda.tf: Set lambda timeout to 600

--- lambda.tf
+++ lambda.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "alb_logs_to_elasticsearch_vpc" {
   filename         = local.lambda_function_filename
   function_name    = "${var.prefix}alb-logs-to-elasticsearch"
   description      = "${var.prefix}alb-logs-to-elasticsearch"
-  timeout          = 300
+  timeout          = 600 # Set this to 10 minutes to avoid timeouts in case of large VPC flow logs
   runtime          = "nodejs${var.nodejs_version}"
   role             = aws_iam_role.role.arn
   handler          = "index.handler"
