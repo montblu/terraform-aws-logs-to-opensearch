8620711191dbfea06a3eec25053566b11a4b07a2
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2024-11-06 21:55:17 +0000

    lambda.tf: Get rid of code not needed

--- lambda.tf
+++ lambda.tf
@@ -1,8 +1,8 @@
-resource "aws_lambda_function" "alb_logs_to_elasticsearch_vpc" {
-  count            = length(var.subnet_ids) > 0 ? 1 : 0
+resource "aws_lambda_function" "alb_logs_to_elasticsearch" {
+  count            = length(var.subnet_ids) == 0 ? 1 : 0
   filename         = local.lambda_function_filename
   function_name    = local.resource_name
-  description      = "${var.name_prefix}alb-logs-to-elasticsearch"
+  description      = local.resource_name
   timeout          = 600  # Set this to 10 minutes to avoid timeouts in case of a large VPC flow logs
   memory_size      = 1024 # Set this to 1024MB to avoid timeouts in case of a large VPC flow logs
   runtime          = "nodejs${var.nodejs_version}"
@@ -10,50 +10,6 @@ resource "aws_lambda_function" "alb_logs_to_elasticsearch_vpc" {
   handler          = "index.handler"
   source_code_hash = filebase64sha256(local.lambda_function_filename)
 
-  environment {
-    variables = {
-      es_endpoint = var.es_endpoint
-      region      = var.region
-    }
-  }
-
-  tags = merge(
-    var.tags,
-    tomap({ "Scope" = "${var.name_prefix}lambda_function_to_elasticsearch" }),
-  )
-
-  # This will be a code block with empty lists if we don't create a securitygroup and the subnet_ids are empty.
-  # When these lists are empty it will deploy the lambda without VPC support.
-  vpc_config {
-    subnet_ids         = var.subnet_ids
-    security_group_ids = [aws_security_group.lambda[0].id]
-  }
-
-  lifecycle {
-    ignore_changes = [filename]
-  }
-}
-
-resource "aws_lambda_permission" "allow_terraform_bucket_vpc" {
-  count         = length(var.subnet_ids) > 0 ? 1 : 0
-  statement_id  = "AllowExecutionFromS3Bucket"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.alb_logs_to_elasticsearch_vpc[0].arn
-  principal     = "s3.amazonaws.com"
-  source_arn    = var.s3_bucket_arn
-}
-
-resource "aws_lambda_function" "alb_logs_to_elasticsearch" {
-  count            = length(var.subnet_ids) == 0 ? 1 : 0
-  filename         = local.lambda_function_filename
-  function_name    = "${var.name_prefix}alb-logs-to-elasticsearch"
-  description      = "${var.name_prefix}alb-logs-to-elasticsearch"
-  timeout          = 300
-  runtime          = "nodejs${var.nodejs_version}"
-  role             = aws_iam_role.role.arn
-  handler          = "index.handler"
-  source_code_hash = filebase64sha256(local.lambda_function_filename)
-
 
   environment {
     variables = {
@@ -64,7 +20,7 @@ resource "aws_lambda_function" "alb_logs_to_elasticsearch" {
 
   tags = merge(
     var.tags,
-    tomap({ "Scope" = "${var.name_prefix}lambda_function_to_elasticsearch" }),
+    tomap({ "Scope" = local.resource_name })
   )
 
   lifecycle {
